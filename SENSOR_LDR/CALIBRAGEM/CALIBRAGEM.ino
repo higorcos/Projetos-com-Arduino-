a205720b7f44e949e80e8ae18fc9bd93626437a6
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-05-31 14:05:02 -0300

    Projetos comentados

---
+++ SENSOR_LDR/CALIBRAGEM/CALIBRAGEM.ino
@@ -0,0 +1,14 @@
+/***************************************\
+** Calibrar LDR **
+\***************************************/
+const int LDR = 0;
+int ValorLido = 0;
+void setup() {
+Serial.begin(9600);
+}
+void loop() {
+ValorLido = analogRead(LDR);
+Serial.print("Valor lido pelo LDR = ");
+Serial.println(ValorLido);
+delay(500);
+}
---
+++ Sem_foro/Sem_foro.ino
@@ -0,0 +1,28 @@
+
+//Definimos os pinos do led
+int VERMELHO = 2;      
+int AMARELO = 4;
+int VERDE = 7;
+ 
+void setup() {
+  pinMode(VERDE,OUTPUT);        //define o pino do led VERDE como saída
+  pinMode(AMARELO,OUTPUT);     //define o pino do led  como saída
+  pinMode(VERMELHO,OUTPUT);         //define o pino do led  como saída
+}
+//Método loop, é executado enquanto o arduino estiver ligado.
+void loop() {
+  //Controle do led verde
+  digitalWrite(VERDE,HIGH);  //acende o led VERDE
+  delay(4000);           //espera 4 segundos
+  digitalWrite(VERDE,LOW);   //apaga o led VERDE
+   
+  //Controle do led amarelo
+  digitalWrite(AMARELO,HIGH);  //acende o led AMARELO
+  delay(2000);           //espera 2 segundos
+  digitalWrite(AMARELO,LOW);   //apaga o led AMARELO
+   
+  //Controle do led vermelho
+  digitalWrite(VERMELHO,HIGH); //acende o led VERMELHO
+  delay(4000);           //espera 4 segundos
+  digitalWrite(VERMELHO,LOW);  //apaga o led VERMELHO
+}
