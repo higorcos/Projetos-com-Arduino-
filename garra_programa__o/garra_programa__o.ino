36d5c16e2113c3e6901c86ffac4cbcfe915eab46
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-05-31 14:06:10 -0300

    Projetos comentados

---
+++ garra_programa__o/garra_programa__o.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo Garra;
+
+int Pot = A0;
+int ValorPot = 0;
+int ValorGarra = 0;
+
+void setup() {
+  Garra.attach(6);//porta
+}
+
+void loop() {
+  ValorPot = analogRead(Pot);
+  ValorGarra = map(ValorPot, 0, 1023, 0, 180);
+  Garra.write(ValorGarra);
+}
